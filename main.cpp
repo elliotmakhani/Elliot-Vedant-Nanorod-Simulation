77cc80e36d5c5fc08a40fa94f4ffbcb5b5ccf1cf
Elliot Makhani <79432614+elliotmakhani@users.noreply.github.com>
2025-11-20 18:25:34 -0800

    convert main to cpp

---
+++ main.cpp
@@ -0,0 +1,27 @@
+// combine runSim and main into one file. always going to be running simulations in cpp, analyze data in python
+// def main(sim):
+//     if sim.mode == 'save':
+//         runSim(sim)
+//     elif sim.mode == 'show':
+//         graphSim(sim)
+
+
+// '''spheresim.readData()
+// spheresim.saveSim()
+// cylindersim.readData()
+// cylindersim.saveSim()'''
+
+// #main(spheresim)
+// main(cylindersim)
+// # main(jobSimcyl)
+// #main(cylindersim)
+// elapsed = time.time()- start
+// cylindersim.updateProgress(f"Elapsed time: {elapsed:.4f} seconds")
+
+// def runSim(sim):
+//     sim.generateData()
+//     sim.updateProgress("data generated")
+//     sim.saveSim()
+//     ''''''
+// #runSim(spheresim)
+// #runSim(cylindersim)
